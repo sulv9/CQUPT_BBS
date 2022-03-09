eec00a108666e8d05cc9b725aea0a07d45c61e95
Tsingshui <tsingshuii@outlook.com>
2022-03-09 11:27:00 +0800

    test

--- bbs/user/models.py
+++ bbs/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+
+
+class Account(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='extended')
+    gender = models.CharField('')
