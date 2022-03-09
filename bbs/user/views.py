77f25b538a660961dd8dad548d936dbcf254e750
Tsingshui <tsingshuii@outlook.com>
2022-03-09 17:29:04 +0800

    。。。

--- bbs/user/views.py
+++ bbs/user/views.py
@@ -1,5 +1,28 @@
+
+from django.contrib.auth import authenticate,login
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
 
-# Create your views here.
+def loginHTML(request):
+    if request.method == 'GET':
+        return render(request,'login.html')
+    elif  request.method =='POST':
+        userName = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username=userName,pass
+                            word=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/user')
+        else:
+            return HttpResponse('Not a user')
 
-def login(request):
+
+# def login(request):
+#     if request.method =='GET':
+#         return render(request,'login.html')
+#     if request.method == 'POST':
+#         # print(request.POST['username')
+#         print('This is POST')
+#         return HttpResponse('post is ok')
