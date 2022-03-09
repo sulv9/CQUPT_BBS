1615b9c44381dd734c6cfa2863270c8c521ea5e8
sulv <sulv1218@gmail.com>
2022-03-09 16:34:33 +0800

    [feat]update Home, model

--- bbs/user/models.py
+++ bbs/user/models.py
@@ -1,4 +1,5 @@
 from operator import mod
+from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -14,8 +15,13 @@ class Article(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
+    short = content[:40] + '...'
     pub_time = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def _str_(self):
         return self.title
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=CASCADE, related_name='extension')
+    avatar = models.IntegerField()
