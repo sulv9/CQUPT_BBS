b2b286b0e23f9ffa63ced79cecc10beac992290c
sulv <sulv1218@gmail.com>
2022-03-09 16:34:33 +0800

    [feat]update Home, model

--- bbs/home/urls.py
+++ bbs/home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'home'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('article/<int:article_id>/', views.article, name='article'),
+    path('topic/<int:topic_id>', views.topic, name="topic")
 ]
--- bbs/user/views.py
+++ bbs/user/views.py
@@ -1,3 +1,9 @@
+from multiprocessing import context
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
+def userHome(request, user_name):
+    user = User.objects.get(username=user_name)
+    context = {'user': user}
+    return render(request, 'user/user.html', context)
